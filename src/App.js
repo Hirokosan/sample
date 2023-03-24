9006109f57046def7ecb98a05b55ce2a485d3adf
Hirokosan <3164721241@qq.com>
2023-03-25 03:24:36 +0800

    commit something

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { HashRouter, Switch, Route, useHistory, useLocation } from "react-router-dom";
+import './App.less';
+import { Button } from 'antd';
+import Home from "./pages/home";
+import GetItem from "./pages/setting";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <HashRouter>
+      <Switch  >
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/setting" component={GetItem}></Route>
+      </ Switch>
+
+    </HashRouter>
   );
 }
 
